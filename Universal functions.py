d51a8ee93894fcf724be56d09a4b5d5df587b9fa
Bhuvanesh <147846242+Bhuvaneshhhh@users.noreply.github.com>
2024-07-30 14:38:14 +0530

    Add files via upload

---
+++ Universal functions.py
@@ -0,0 +1,18 @@
+import numpy as np
+arr=np.array([1, 2, 3, 4, 5])
+
+sqrt_arr = np.sqrt(arr)
+print(sqrt_arr)
+
+exp_arr = np.exp(arr)
+print(exp_arr)
+
+sin_arr = np.sin(arr)
+print(sin_arr)
+
+sum_arr = np.sum(arr)
+print(sum_arr)
+
+mean_arr = np.mean(arr)
+print(mean_arr)
+
