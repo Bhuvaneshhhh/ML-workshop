b83c5ca20ba5ef87c4934143e0d6486e45c3616c
Bhuvanesh <147846242+Bhuvaneshhhh@users.noreply.github.com>
2024-08-02 10:23:45 +0530

    Add files via upload

---
+++ Day 4/Violin plot.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+tips = sns.load_dataset("tips")
+sns.violinplot(x='day', y='total_bill', data=tips)
+plt.title("Violin plot")
+plt.show()
